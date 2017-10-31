426f63a7ce5979433695d06b334c77e59bda8991
Christian Fengler <christian.fengler@campus.tu-berlin.de>
2017-10-31 19:45:50 +0100

    Java-Maven-Projekt angelegt und Neo4J-Abhängigkeit hinzugefügt

---
+++ kg_neo4j/src/main/java/tub/dbpro/ws1718/kg_neo4j/App.java
@@ -0,0 +1,13 @@
+package tub.dbpro.ws1718.kg_neo4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
